338ab3a16ea6cd769d7859ac159ef3b041ee84a6
Lee byeoksan <lee.byeoksan+bitbucket@gmail.com>
2016-12-20 20:31:39 +0900

    UPDATE prune method (qfactor, ratio). Previous 'prune' method becomes 'prune_qfactor'

--- layers/SpatialConvolutionWithMask.lua
+++ layers/SpatialConvolutionWithMask.lua
@@ -57,32 +57,92 @@ function SpatialConvolutionWithMask:applyBiasMask()
     end
 end
 
-function SpatialConvolutionWithMask:prune(qfactor)
+function SpatialConvolutionWithMask:get_alive_weights()
     local alive_weights = self.weight[self.weightMask:eq(1)]
-    if self.bias then
-        local alive_bias = self.bias[self.biasMask:eq(1)]
-        if alive_bias:dim() > 0 then
-            alive_weights = alive_weights:cat(alive_bias)
-        end
-    end
-
     if alive_weights:dim() == 0 then
-        return
+        return nil
     end
 
-    local mean = alive_weights:mean()
-    local std = alive_weights:std()
-    local threshold = torch.abs(qfactor) * std
+    return alive_weights
+end
 
-    local new_weight_mask = torch.abs(self.weight - mean):ge(threshold)
-    self.weightMask[new_weight_mask:eq(0)] = 0
+function SpatialConvolutionWithMask:get_alive_biases()
+    if not self.bias then
+        return nil
+    end
+
+    local alive_biases = self.bias[self.biasMask:eq(1)]
+    if alive_biases:dim() == 0 then
+        return nil
+    end
+
+    return alive_biases
+end
+
+function SpatialConvolutionWithMask:get_alive()
+    local alive_weights = self:get_alive_weights()
+    local alive_biases = self:get_alive_biases()
+    local alive
+
+    if alive_weights ~= nil and alive_biases ~= nil then
+        alive = alive_weights:cat(alive_biases)
+    elseif alive_biases == nil then
+        alive = alive_weights
+    elseif alive_weights == nil then
+        alive = alive_biases
+    else
+        return nil
+    end
+
+    return alive -- 1-D
+end
+
+function SpatialConvolutionWithMask:prune_range(lower, upper)
+    if lower > upper then
+        lower, upper = upper, lower
+    end
+
+    local new_weight_mask = torch.cmul(self.weight:ge(lower), self.weight:le(upper))
+    self.weightMask[new_weight_mask:eq(1)] = 0
     self:applyWeightMask()
 
     if self.bias then
-        local new_bias_mask = torch.abs(self.bias - mean):ge(threshold)
-        self.biasMask[new_bias_mask:eq(0)] = 0
+        local new_bias_mask = torch.cmul(self.bias:ge(lower), self.bias:le(upper))
+        self.biasMask[new_bias_mask:eq(1)] = 0
         self:applyBiasMask()
     end
+end
+
+function SpatialConvolutionWithMask:prune_qfactor(qfactor)
+    local alive = self:get_alive()
+    if alive == nil then
+        return
+    end
+
+    local mean = alive:mean()
+    local std = alive:std()
+    local range = torch.abs(qfactor) * std
+
+    lower, upper = mean - range, mean + range
+    self:prune_range(lower, upper)
+end
+
+function SpatialConvolutionWithMask:prune_ratio(ratio)
+    if ratio > 1 or ratio < 0 then
+        return
+    end
+
+    local alive = self:get_alive()
+    alive = alive:abs():sort()
+    local idx = math.floor(alive:size(1) * ratio)
+
+    if idx == 0 then
+        return
+    end
+
+    local range = alive[idx]
+    lower, upper = -range, range
+    self:prune_range(lower, upper)
 end
 
 function SpatialConvolutionWithMask:updateOutput(input)
